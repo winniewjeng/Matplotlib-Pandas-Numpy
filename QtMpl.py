7caa72bcb7b9036d6cc26dae9677d5691d1c3d58
Winnie Jeng <winniewjeng@gmail.com>
2018-12-06 01:14:36 -0800

    work on qtmpl.py addbar method

--- QtMpl.py
+++ QtMpl.py
@@ -6,12 +6,32 @@ from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
 
 
 class QtMpl(FigureCanvasQTAgg):
+    """This class is in charge of the Matplotlib plotting!!!"""
 
-    '''
-    '''
 
     def __init__(self, parent):
+
+        """
+        Constructor
+        """
+
         self.fig = matplotlib.figure.Figure()
-        self.setParent(parent)
+        self.axes = self.fig.add_subplot(111)
+
         FigureCanvasQTAgg.__init__(self, self.fig)
 
+        self.setParent(parent)
+
+        self.axes.set_ylabel("Money")
+        self.axes.set_xlabel("Dates")
+        self.axes.set_title("Movie Revenue by Month")
+
+        # define the widget as expandable
+        FigureCanvasQTAgg.setSizePolicy(self,
+                                        PyQt5.QtWidgets.QSizePolicy.Expanding,
+                                        PyQt5.QtWidgets.QSizePolicy.Expanding)
+
+        # notify the system of updated policy
+        FigureCanvasQTAgg.updateGeometry(self)
+
+    def addLine(self, x=None, revenue=None, budget=None, year=None):
